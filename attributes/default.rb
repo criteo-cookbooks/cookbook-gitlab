748db798accbe6c19ec0f5ed2750b3a3b761605b
Maxime Brugidou <m.brugidou@criteo.com>
2014-03-25 22:51:04 +0100

    WIP using omnibus install
    Since this is a complete rewrite we might move it to a new cookbook
    NOTE: it does not support HTTPS yet
    (waiting for 6.7
    https://gitlab.com/gitlab-org/omnibus-gitlab/merge_requests/66 )

--- attributes/default.rb
+++ attributes/default.rb
@@ -18,6 +18,28 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+default['gitlab']['version'] = '6.9.0'
+
+case platform
+when 'debian'
+  default['gitlab']['package']['url'] = "https://downloads-packages.s3.amazonaws.com/debian-7.4"
+  default['gitlab']['package']['filename'] = "gitlab_#{node['gitlab']['version']}-omnibus.2-1_amd64.deb"
+  default['gitlab']['package']['checksum'] = '38da9119c25a6f1f4ece4355dcad3acf1a5d9c8a6fa19a25e40b7b7ec843f046'
+when 'ubuntu'
+  default['gitlab']['package']['url'] = "https://downloads-packages.s3.amazonaws.com/ubuntu-12.04"
+  default['gitlab']['package']['filename'] = "gitlab_#{node['gitlab']['version']}-omnibus-1_amd64.deb"
+  default['gitlab']['package']['checksum'] = '42e8224f8aa8689ba80380d036a3b367ffb63a85b5e447670a5233d888b85924'
+when 'rhel', 'centos'
+  default['gitlab']['package']['url'] = "https://downloads-packages.s3.amazonaws.com/centos-6.5"
+  default['gitlab']['package']['filename'] = "gitlab-#{node['gitlab']['version']}_omnibus-1.el6.x86_64.rpm"
+  default['gitlab']['package']['checksum'] = '069067225494df842d4c724667282a0665de832b11fb044b0493cf15070b178a'
+end
+
+# Config in /etc/gitlab/gitlab.rb
+# default['gitlab']['config']['git_data_dir'] = "/my/dir"
+# default['gitlab']['config']['gitlab_rails']['giltab_https'] = true
+
+
 # Set attributes for the git user
 default['gitlab']['user'] = "git"
 default['gitlab']['group'] = "git"
--- recipes/default.rb
+++ recipes/default.rb
@@ -18,6 +18,40 @@
 # limitations under the License.
 #
 
+pkg_path = "#{Chef::Config[:file_cache_path]}/#{node['gitlab']['package']['filename']}"
+
+remote_file pkg_path do
+  source "#{node['gitlab']['package']['url']}/#{node['gitlab']['package']['filename']}"
+  checksum node['gitlab']['package']['checksum']
+end
+
+package 'gitlab' do
+  source pkg_path
+  notifies :run, 'execute[reconfigure-gitlab]'
+end
+
+directory '/etc/gitlab' do
+  owner 'root'
+  group 'root'
+end
+
+template '/etc/gitlab/gitlab.rb' do
+  owner 'root'
+  group 'root'
+  source 'gitlab.rb.erb'
+  variables(
+    :gitlab_config => node['gitlab']['config'] || {}
+  )
+  notifies :run, 'execute[reconfigure-gitlab]'
+end
+
+execute 'reconfigure-gitlab' do
+  command 'gitlab-ctl reconfigure'
+  action :nothing
+end
+
+return
+
 case node['platform_family']
 when 'debian'
   include_recipe 'apt'
